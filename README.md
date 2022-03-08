e8a14d11578913179f0e4e4ee9ba9ef535043c35
yakyouk <louis.blesch@gmail.com>
2022-03-08 12:12:58 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# CORS PROXY
+A simple cors proxy based on cors-anywhere
+
+### Run
+- Create a `certs` folder and place into it ssl certificate files `cert.pem` and `key.pem`
+- Run `PORT=6300 node app.js`
---
+++ app.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+
+// Listen on a specific host via the HOST environment variable
+var host = process.env.HOST || "0.0.0.0";
+// Listen on a specific port via the PORT environment variable
+var port = process.env.PORT || 8080;
+
+var cors_proxy = require("cors-anywhere");
+cors_proxy
+  .createServer({
+    originWhitelist: [], // Allow all origins
+    httpsOptions: {
+        key: fs.readFileSync("./certs/key.pem"),
+        cert: fs.readFileSync("./certs/cert.pem"),
+    },
+    // requireHeader: ["authorization"],
+    // removeHeaders: ['cookie', 'cookie2']
+  })
+  .listen(port, host, function () {
+    console.log("Running CORS Anywhere on " + host + ":" + port);
+  });
